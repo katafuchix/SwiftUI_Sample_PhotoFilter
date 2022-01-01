72954d034d86475b78f15f2e93ee58000309abbb
katafuchix <katafuchix@gmail.com>
2022-01-01 14:08:55 +0900

    Initial Commit

---
+++ SwiftUI_Sample_PhotoFilter/SwiftUI_Sample_PhotoFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Sample_PhotoFilterApp.swift
+//  SwiftUI_Sample_PhotoFilter
+//
+//  Created by cano on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Sample_PhotoFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
